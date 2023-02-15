e485e2550c1b2aee83c38a4b60919e11b9b3d2f7
John Lindquist <johnlindquist@gmail.com>
2023-02-14 17:38:31 -0700

    feat(init): hello world

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,125 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next"
+import { useCallback, useEffect, useRef, useState } from "react"
+import Peer from "simple-peer"
 
 const Home: NextPage = () => {
+  const peerRef = useRef<Peer.Instance>()
+  const [offer, setOffer] = useState("")
+  const [answer, setAnswer] = useState("")
+  const [initiator, setInitiator] = useState(false)
+
+  useEffect(() => {
+    if (location.search.includes("init")) {
+      setInitiator(true)
+      console.log(`I'm the initiator`)
+    } else {
+      console.log(`I'm not the initiator`)
+    }
+  }, [])
+
+  useEffect(() => {
+    peerRef.current = new Peer({
+      initiator,
+      trickle: false,
+    })
+
+    return () => {
+      peerRef.current?.destroy()
+    }
+  }, [initiator])
+
+  useEffect(() => {
+    if (!peerRef.current) return
+
+    const peer = peerRef.current
+
+    console.log(`Peer created. Initiator: ${initiator}`)
+    peer.on("signal", data => {
+      console.log(`Type: ${data.type}`, data)
+      if (data.type === "offer") {
+        setOffer(JSON.stringify(data))
+      } else if (data.type === "answer") {
+        setAnswer(JSON.stringify(data))
+      }
+    })
+
+    peer.on("connect", () => {
+      console.log("connected")
+      peer.send(`Hello from the ${initiator ? "initiator" : "receiver"}`)
+    })
+
+    peer.on("error", err => {
+      console.log("error", err)
+    })
+
+    peer.on("data", data => {
+      console.log("data", data.toString())
+    })
+
+    return () => {
+      peer.destroy()
+    }
+  }, [initiator])
+
+  const peerSend = useCallback((data: any) => {
+    if (!peerRef.current) return
+    peerRef.current.send(data)
+  }, [])
+
+  const peerSignal = useCallback((data: any) => {
+    if (!peerRef.current) return
+    console.log(`Signaling offer:`, data)
+
+    peerRef.current.signal(typeof data === "string" ? JSON.parse(data) : data)
+  }, [])
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="flex h-screen flex-row items-center justify-evenly py-2">
+      {/* Create a textarea to display the offer */}
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <div className="flex flex-col h-1/2">
+        <textarea
+          className="border border-gray-300 rounded p-2 min-w-screen font-mono text-xs h-full"
+          value={offer}
+          onChange={e => setOffer(e.target.value)}
+        />
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+          onClick={() => {
+            if (initiator) {
+              navigator.clipboard.writeText(offer)
+            } else {
+              peerSignal(offer)
+            }
+          }}
         >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+          {initiator ? "Copy offer to clipboard" : "Signal Offer Received"}
+        </button>
+      </div>
+
+      <div className="flex flex-col h-1/2">
+        {/* Create a textarea to display the answer */}
+        <textarea
+          className="border border-gray-300 rounded p-2 min-w-screen font-mono text-xs h-full"
+          value={answer}
+          onChange={e => setAnswer(e.target.value)}
+        />
+
+        {/* Create a button to signal the offer */}
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+          onClick={() => {
+            if (initiator) {
+              peerSignal(answer)
+            } else {
+              navigator.clipboard.writeText(answer)
+            }
+          }}
+        >
+          {initiator ? "Signal Answer Received" : "Copy answer to clipboard"}
+        </button>
+      </div>
     </div>
   )
 }
